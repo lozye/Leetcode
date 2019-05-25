6540995af80cee1cc915baa6d4bf4054383d734c
lozye <lozye@qq.com>
2019-05-25 16:04:15 +0800

    v20190525-twosum

---
+++ src/Leetcode/Lession/two-sum/Twosum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Lession.two_sum
+{
+    class Twosum
+    {
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+
+
+        }
+    }
+}
---
+++ src/Leetcode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Lession.two_sum.Twosum().TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+        }
+    }
+}
